b38ea2857e2fa9cc32665e13ff0f7825ec3263ec
June Zhang <151397065+junebuggy6moon@users.noreply.github.com>
2024-02-17 19:53:15 -0800

    p14p3.py

--- p14p3.py
+++ p14p3.py
@@ -4,11 +4,18 @@
 
 user_num = input("Please enter a number: ")
 int_num = int(user_num)
+if int_num <= 0:
+    print("The number must be positive!")
 
-for i in range(2, int_num):
-    if int_num % i == 0:
-        print(f"{int_num} It's not prime number")
-        break
-else:
-    print(f"{int_num} It's a prime number")
+elif int_num == 1:
+    print(f"{int_num} It's not prime number")
 
+elif int_num > 1:
+
+    for i in range(2, int_num):
+
+        if int_num % i == 0:
+            print(f"{int_num} It's not prime number")
+            break
+    else:
+        print(f"{int_num} It's a prime number")
