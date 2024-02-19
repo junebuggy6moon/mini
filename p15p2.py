1f056350bdd5922b5f47ce1842c94e04a7553fa5
June Zhang <151397065+junebuggy6moon@users.noreply.github.com>
2024-02-18 18:55:34 -0800

    Practical Sheet 15

---
+++ p15p2.py
@@ -0,0 +1,18 @@
+
+def f(n):
+    if n == 1:
+        print("f(n) = 1")
+        return 1
+
+    elif n > 1:
+        answer = f(n - 1) + 2 ** (n - 1)
+        print(f"f({n}) = f({n} - 1) + 2 ** ({n} - 1) f = {(answer - 1) + 2 ** (n - 1)} = {answer}")
+        return answer
+
+user_input = input("Please enter an number: ")
+int_input = int(user_input)
+
+if int_input < 1:
+    print("The number you entered must be equal or greater than 1.")
+
+print(f(int_input))
