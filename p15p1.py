0c8016c07f3c376bf0a589b3a4514fc5204c8c72
June Zhang <151397065+junebuggy6moon@users.noreply.github.com>
2024-02-18 18:04:52 -0800

    Practical Sheet 15

---
+++ p15p1.py
@@ -0,0 +1,17 @@
+def f(n):
+    if n == 1:
+        print("f(1) = 2")
+        return 2
+    elif n > 1:
+        answer = n + f(n - 1)
+        print(f"f({n}) = {n} + f({n-1}) = {n} + {answer - n} = {answer}")
+        return answer
+
+user_input = input("Please enter a number: ")
+int_input = int(user_input)
+
+if int_input <= 1:
+    print("The number you entered must be greater than 1.")
+
+print(f(int_input))
+
