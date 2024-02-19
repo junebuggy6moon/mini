7e431df9945a577de574077aef618c34cf687e52
June Zhang <151397065+junebuggy6moon@users.noreply.github.com>
2024-02-18 16:36:56 -0800

    Practical Sheet 14

--- p14q1_recur.py
+++ p14q1_recur.py
@@ -3,10 +3,9 @@ def find(a_list, finding_element):
     if not a_list:
         return False
 
-    if a_list == finding_element:
+    if a_list[0] == finding_element:
         return True
 
-    # Call the function recursively until it run through the rest of the elements.
     return find(a_list[1:], finding_element)
 
 l = [2, 4, 6, 8, 10]
